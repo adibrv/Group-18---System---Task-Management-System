b5c9028b5b3792aa6477797dca9f3b86c943a982
adibrv <adibruv@gmail.com>
2024-02-27 19:43:08 +0800

    +db connection +list refresh +add button command

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+import pymysql
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 from tkcalendar import DateEntry
+
+# Connect to DB
+
+
+def connectdb():
+    conn = pymysql.connect(
+        host='localhost', user='root', password='', db='tasklist'
+    )
+    return conn
+
+
+def table_refresh():
+    for data in tms_tree.get_children():
+        tms_tree.delete(data)
+
+    for array in read():
+        tms_tree.insert(parent='', index='end', iid=array, text="", values=array, tag='row')
+
+    tms_tree.tag_configure('row', background='#EEEEEE', font=('Arial', 12))
+    tms_tree.grid(row=10, column=0, columnspan=15, rowspan=11, padx=50, pady=20)
+
 
 # GUI Window
 gui = Tk()
@@ -9,6 +33,40 @@ gui.geometry("1024x768")
 tms_tree = ttk.Treeview(gui)
 
 # functions content
+
+
+def read():
+    conn = connectdb()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    results = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return results
+
+
+def add():
+    priority = str(taskPriorityEntry.get())
+    title = str(taskNameEntry.get())
+    date = str(taskDateEntry.get())
+    time = str(taskTimeHour.get()) + str(taskTimeAMPM.get())
+
+    if (priority == '' or priority == ' ') or (title == '' or title == ' ') or (date == '' or date == ' ') or (time == '' or time == ' '):
+        messagebox.showinfo("Error", "One or more entries are blank")
+        return
+    else:
+        try:
+            conn = connectdb()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO tasks VALUES ('"+priority+"', '"+title+"', '"+date+"', '"+time+"')")
+            conn.commit()
+            conn.close()
+        except:
+            messagebox.showinfo("Error", "Task name already exists")
+            return
+
+    table_refresh()
+
 
 # GUI contents
 header = Label(gui, text="Task Management System", font=('Arial Bold', 30))
@@ -37,7 +95,7 @@ taskPriorityEntry = ttk.Combobox(gui, values=['Low', 'Medium', 'High'])
 taskPriorityEntry.grid(row=6, column=1, columnspan=1, padx=5, pady=0)
 
 
-addBtn = ttk.Button(gui, text='Add', width=10)
+addBtn = ttk.Button(gui, text='Add', width=10, command=add)
 addBtn.grid(row=3, column=6, columnspan=1, rowspan=2, padx=35, pady=0)
 updateBtn = ttk.Button(gui, text='Update', width=10)
 updateBtn.grid(row=4, column=6, columnspan=1, rowspan=2, padx=35, pady=0)
@@ -58,6 +116,6 @@ tms_tree.heading("Date", text="Date", anchor=W)
 tms_tree.column("Time", anchor=W, width=150)
 tms_tree.heading("Time", text="Time", anchor=W)
 tms_tree.grid(row=10, column=0, columnspan=15, rowspan=11, padx=50, pady=20)
-
+table_refresh()
 
 gui.mainloop()
